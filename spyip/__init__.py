2d6c18f1b1c394f1f926b0e6e4069cf3e3741a6d
Md. Almas Ali <almaspr3@gmail.com>
2024-01-01 01:39:24 +0600

    add: main package file.

---
+++ spyip/__init__.py
@@ -0,0 +1,64 @@
+"""
+SpyIP - A simple IP lookup tool written in Python.
+
+Dependent on:
+-------------
+    httpx
+    pydantic
+    https://ip-api.com/
+    
+
+Supported fields:
+-----------------
+    status
+    continent
+    continentCode
+    country
+    countryCode
+    region
+    regionName
+    city
+    district
+    zip
+    lat
+    lon
+    timezone
+    offset
+    currency
+    isp
+    org
+    as
+    asname
+    mobile
+    proxy
+    hosting
+    query
+
+    
+Supported languages:
+--------------------
+
+| lang (ISO 639) | description                     |
+| -------------- | ------------------------------- |
+| en             | English (default)               |
+| de             | Deutsch (German)                |
+| es             | Español (Spanish)               |
+| pt-BR          | Português - Brasil (Portuguese) |
+| fr             | Français (French)               |
+| ja             | 日本語 (Japanese)               |
+| zh-CN          | 中国 (Chinese)                  |
+| ru             | Русский (Russian)               |
+
+"""
+
+from .backend import trace_me, trace_ip, trace_dns, trace_ip_batch
+
+
+__version__ = '0.1.0'
+__author__ = 'Md. Almas Ali'
+__all__ = [
+    'trace_me',
+    'trace_ip',
+    'trace_dns',
+    'trace_ip_batch',
+]
